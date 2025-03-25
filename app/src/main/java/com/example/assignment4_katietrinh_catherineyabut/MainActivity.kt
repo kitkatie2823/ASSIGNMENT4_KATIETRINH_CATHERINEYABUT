179764ff5d7cfac7da52db4140a8aa62a793b050
catyabut36 <catherineyabut1837@gmail.com>
2025-03-25 12:39:09 -0700

    Fixed firebase sdk added

--- app/src/main/java/com/example/assignment4_katietrinh_catherineyabut/MainActivity.kt
+++ app/src/main/java/com/example/assignment4_katietrinh_catherineyabut/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val db = Firebase.firestore
         setContentView(R.layout.activity_main)
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
